04ba1b627272cdb49f1993d65182bc1b3c29a8ec
wecoastca <foxconq@gmail.com>
2020-05-03 16:51:37 +0300

    Calculate probability

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,5 +1,21 @@
+import data from '../data';
+
+type Question = [string, number, number]
+
+type PredictData = Question[]
+
 const main = async () => {
-  console.log('Hello from core');
-};
+  //@ts-ignore
+  await estimateCovid(data);
+}
+//функция, которая принимает признак, вес, вес ответа и возвращает вероятность
+const estimateCovid = (data:PredictData) => {
+
+  const overallProbability = data
+  .reduce((prob, current) => prob + current[1] * current[2] ,0);
+
+  return overallProbability;
+
+}
 
 main();
