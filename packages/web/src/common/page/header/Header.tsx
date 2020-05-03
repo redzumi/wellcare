979e258a5e7903b8d6066ca89156cb3ca702e6ff
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-05-03 19:53:47 +0300

    Wow, looks good

--- packages/web/src/common/page/header/Header.tsx
+++ packages/web/src/common/page/header/Header.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
-import { Row, Col, Layout, Breadcrumb } from 'antd';
+import React, { useState } from 'react';
+import {
+  Drawer,
+  Avatar,
+  Badge,
+  Space,
+  Button,
+  Row,
+  Col,
+  Layout,
+  Breadcrumb,
+  Popover,
+  Empty
+} from 'antd';
+import {
+  UserOutlined,
+  MenuOutlined,
+  NotificationOutlined
+} from '@ant-design/icons';
 
 import styles from './styles.styl';
 
 const { Header } = Layout;
 
 const PageHeader = () => {
+  const [drawer, setDrawer] = useState(false);
+
+  const handleMenuClick = () => setDrawer(true);
+  const handleDrawerClose = () => setDrawer(false);
+
   return (
-    <Header className={styles.header}>
-      <Row justify="space-between" className={styles.row}>
-        <Col>
-          <Breadcrumb className={styles.breadcrumb}>
-            <Breadcrumb.Item>Wellcare</Breadcrumb.Item>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-          </Breadcrumb>
-        </Col>
-        <Col>Test</Col>
-      </Row>
-    </Header>
+    <React.Fragment>
+      <Header className={styles.header}>
+        <Row justify="space-between" className={styles.row}>
+          <Col>
+            <Breadcrumb className={styles.breadcrumb}>
+              <Breadcrumb.Item>Wellcare</Breadcrumb.Item>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+          <Col>
+            <Space align="center">
+              <Avatar className={styles.avatar} icon={<UserOutlined />} />
+              <Popover
+                placement="bottomRight"
+                content={<Empty />}
+                trigger="click"
+              >
+                <Badge dot>
+                  <Button icon={<NotificationOutlined />} />
+                </Badge>
+              </Popover>
+              <Button icon={<MenuOutlined />} onClick={handleMenuClick} />
+            </Space>
+          </Col>
+        </Row>
+      </Header>
+      <Drawer placement="right" onClose={handleDrawerClose} visible={drawer}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </React.Fragment>
   );
 };
 
