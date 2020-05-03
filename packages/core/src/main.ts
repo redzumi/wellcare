f24b7140ef09b99684fcca075a1b5c1cdb4c63ea
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-05-03 18:00:23 +0300

    Add prettier for core package

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,12 +1,15 @@
-import data from '../data';
-import {PredictData} from '../data';
+import data from "../data";
+import { PredictData } from "../data";
 
 const main = async () => await estimateCovid(data);
 
 //функция, которая принимает признак, вес, вес ответа и возвращает вероятность
-const estimateCovid = (data:PredictData) => {
-  const overallProbability = data.reduce((prob, current) => prob + current[1] * current[2] ,0);
+const estimateCovid = (data: PredictData) => {
+  const overallProbability = data.reduce(
+    (prob, current) => prob + current[1] * current[2],
+    0
+  );
   return overallProbability;
-}
+};
 
 main();
