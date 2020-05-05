2d95fd10bc7526ab0b674c4818f5c5322cfc7af8
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-05-05 07:48:47 +0300

    Fixes

--- packages/web/src/modules/surveys/finish/FinishSurvey.tsx
+++ packages/web/src/modules/surveys/finish/FinishSurvey.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { Result, Button } from 'antd';
+import { Result, Card, Row, Space, Divider, Statistic } from 'antd';
 import { useParams } from 'react-router-dom';
 import { useStore } from 'effector-react';
-
+import { ArrowUpOutlined } from '@ant-design/icons';
 import { $surveys, fetchSurveys } from 'store/surveys';
 import Paper from 'common/page/paper/Paper';
 
@@ -19,16 +19,35 @@ const FinishSurvey = () => {
   }, [ready]);
 
   return (
-    <Paper>
-      <Result
-        status="success"
-        title={`Вы успешно прошли: ${currentSurvey?.name}!`}
-        extra={[
-          <Button type="primary" key="results">
-            Узнать результаты
-          </Button>
-        ]}
-      />
+    <Paper title={currentSurvey?.name}>
+      <Result status="success" />
+      <Divider />
+      <Row justify="center">
+        <Space>
+          <Card>
+            <Statistic
+              title="Заболело"
+              value={9.3}
+              precision={2}
+              valueStyle={{ color: '#cf1322' }}
+              prefix={<ArrowUpOutlined />}
+              suffix=".тыс"
+            />
+          </Card>
+          <Card>
+            <Statistic
+              title="Выздоровело"
+              value={11.28}
+              precision={2}
+              valueStyle={{ color: '#3f8600' }}
+              prefix={<ArrowUpOutlined />}
+              suffix=".тыс"
+            />
+          </Card>
+        </Space>
+        <Divider />
+        <Row>Ваша группа риска:</Row>
+      </Row>
     </Paper>
   );
 };
