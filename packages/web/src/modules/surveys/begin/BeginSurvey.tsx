86cff2a6c60e46f1c30487d3e119fd1a4806d43b
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-05-05 06:55:08 +0300

    Improve questions

--- packages/web/src/modules/surveys/begin/BeginSurvey.tsx
+++ packages/web/src/modules/surveys/begin/BeginSurvey.tsx
@@ -1,50 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Descriptions, Button, Space } from 'antd';
 import { useParams } from 'react-router-dom';
-import Paper from 'common/page/paper/Paper';
-
 import { useStore } from 'effector-react';
 
-import { $surveys } from 'store/surveys';
+import { $surveys, fetchSurveys } from 'store/surveys';
 
+import Paper from 'common/page/paper/Paper';
 import SurveyQuestions from './SurveyQuestions';
 
 import styles from './styles.styl';
 
 const BeginSurvey = () => {
   const { id } = useParams();
-  const { data: surveys } = useStore($surveys);
+  const { ready, data: surveys } = useStore($surveys);
   const currentSurvey = surveys.find((survey) => survey.id === id);
 
   const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    if (!ready) {
+      fetchSurveys();
+    }
+  }, [ready]);
 
   const handleStart = () => {
     setIsStarted(true);
   };
 
-  if (!isStarted) {
-    return (
-      <Paper>
-        <Space align="center" direction="vertical" size="middle">
-          <Card
-            title={currentSurvey?.name}
-            bordered={false}
-            className={styles.cardTitle}
-          >
-            <Descriptions layout="vertical">
-              <Descriptions.Item>
-                {currentSurvey?.description}
-              </Descriptions.Item>
-            </Descriptions>
-          </Card>
-          <Button type="primary" onClick={handleStart}>
-            Начать
-          </Button>
-        </Space>
-      </Paper>
-    );
+  if (isStarted) {
+    return <SurveyQuestions />;
   }
-  return <SurveyQuestions />;
+
+  return (
+    <Paper>
+      <Space align="center" direction="vertical" size="middle">
+        <Card
+          title={currentSurvey?.name}
+          bordered={false}
+          className={styles.cardTitle}
+        >
+          <Descriptions layout="vertical">
+            <Descriptions.Item>{currentSurvey?.description}</Descriptions.Item>
+          </Descriptions>
+        </Card>
+        <Button type="primary" onClick={handleStart}>
+          Начать
+        </Button>
+      </Space>
+    </Paper>
+  );
 };
 
 export default BeginSurvey;
